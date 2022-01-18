6b772e75d8c4d8e189bee1f04b99dbe0633a4a14
kodluyoruz <ensarsehitoglu25@gmail.com>
2022-01-18 12:30:52 +0300

    ilk commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.get('/',(req,res)=>{
+  const photo = {
+    id:1,
+    name:"photo name",
+    description: "photo description"
+  }
+  res.send(photo)
+})
+
+const port = 3000
+app.listen(port,()=>{
+  console.log(`Sunucu ${port} portunda başlatıldı`)
+})
