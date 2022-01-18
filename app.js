7d0cd73a9b3d5c7b3d76179acce08bed9336fafc
kodluyoruz <ensarsehitoglu25@gmail.com>
2022-01-18 18:30:58 +0300

    ödev 8

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express')
+const ejs = require('ejs')
 const app = express()
+const path = require('path')
 
+// template engine
+app.set('view engine','ejs')
+
+// middlewares
+app.use(express.static('public'))
+
+// routes
 app.get('/',(req,res)=>{
-  const blog = { id: 1, title: "Blog title", description: "Blog description" }
-  res.send(blog)
+  res.render('index')
+})
+app.get('/about',(req,res)=>{
+  res.render('about')
+})
+app.get('/add_post',(req,res)=>{
+  res.render('add_post')
+})
+app.get('/post',(req,res)=>{
+  res.render('post')
 })
 
 const port = 3000
